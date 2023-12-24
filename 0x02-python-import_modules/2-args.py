5d15957004cf1c12741401c244aaf664fa43f594
Toluwaloju0 <teecoded1990@gmail.com>
2023-12-24 19:49:30 +0100

    programmes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("{} arguments.".format(len(argv) - 1))
         exit(0)
+    elif len(argv) == 2:
+        print("{} argument".format(len(argv) - 1))
     else:
         print("{} arguments:".format(len(argv) - 1))
     for i in range(1, len(argv)):
