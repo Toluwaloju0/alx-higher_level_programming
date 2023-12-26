c72f9dacdf9629e250b27879ce21f36b2002f305
Toluwaloju0 <teecoded1990@gmail.com>
2023-12-26 13:43:48 +0100

    palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+* is_palindrome - to check if a list is empty
+* @head: pointer to the list
+* Return: 0 if empty, 1 if not empty
+*/
+
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL)
+	{
+		return(0);
+	}
+	return(1);
+}
