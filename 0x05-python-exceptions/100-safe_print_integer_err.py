93e57c3a68b196a2dfb8aec270c0f07237322c65
Toluwaloju0 <teecoded1990@gmail.com>
2024-01-08 23:07:41 +0100

    0x05. Python - Exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except TypeError as te:
-        print("Exception: ", te, file=sys.stderr)
+        print("Exception:", te, file=sys.stderr)
     except ValueError as ve:
-        print("Exception: ", ve, file=sys.stderr)
+        print("Exception:", ve, file=sys.stderr)
         return False
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,4 +4,4 @@ def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as e:
-        print("Exception: ", e, file=sys.stderr)
+        print("Exception:", e, file=sys.stderr)
