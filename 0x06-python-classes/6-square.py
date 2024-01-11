22d8dd6cde2c7c125cc7d4e7f81fc1d7e4bf6ade
Toluwaloju0 <teecoded1990@gmail.com>
2024-01-11 13:19:19 +0100

    0x06. Python - Classes and Objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,6 +46,8 @@ class Square:
     def position(self, value):
         if len(value) != 2 or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
@@ -67,4 +69,3 @@ class Square:
             for b in range(self.__size):
                 print("#", end="")
             print("")
-        return ("")
