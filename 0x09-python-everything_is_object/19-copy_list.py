cea66d849b076a04bd55f1e52ff39806a9151bb5
Toluwaloju0 <teecoded1990@gmail.com>
2024-01-18 09:50:44 +0100

    more objects

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A module to copy a string"""
+
+
+def copy_list(l):
+    """To copy a list
+    Args:
+        l (list): the list to bo copied
+    """
+    n = l[:]
+    return n
