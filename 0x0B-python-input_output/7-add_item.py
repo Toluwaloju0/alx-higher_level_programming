0407f93d4c3cfd5b6966dec5c3c62c9d1135a014
Toluwaloju0 <teecoded1990@gmail.com>
2024-01-22 22:55:03 +0100

    input, output and json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,6 +6,15 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+my_list = []
+a = 1
 
-for a in sys.argv:
-    save_to_json_file(a, "add_item.json")
+try:
+    my_list = load_from_json_file("add_item.json")
+except Exception:
+    pass
+finally:
+    for a in range(1, len(sys.argv)):
+        my_list.append(sys.argv[a])
+
+    save_to_json_file(my_list, "add_item.json")
