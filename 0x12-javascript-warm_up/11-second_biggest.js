638477889e27824e1ac4c75d0f0fb492659cf24a
Toluwaloju0 <teecoded1990@gmail.com>
2024-03-16 16:54:29 +0100

    javascript

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,4 +3,5 @@ const { argv } = ('node:process');
 if (argv.length <= 3) {
   console.log('0');
 } else {
+  for (let a = 0; a < argv.length; a++)
   
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-let a = parseInt(argv[2]);
-let b = 0;
-let c = 0;
+const a = parseInt(argv[2]);
+const c = 'X'.repeat(a);
 if (isNaN(a)) {
   console.log('missing size');
 } else {
   for (let b = 0; b < a; b++) {
-    for(let c = 0; c < a; c++) {
-      console.log('X');
-    }
+    console.log(c);
   }
 }
