ae0ad66a8269629ac206a51255fb3d0e4dcc3258
Toluwaloju0 <teecoded1990@gmail.com>
2024-03-16 20:46:07 +0100

    javascript

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || typeof(w) === 'undefined' || h <= 0 || typeof(h) === 'undefined') {
+    if (w <= 0 || typeof (w) === 'undefined' || h <= 0 || typeof (h) === 'undefined') {
       return null;
     } else {
       this.width = w;
