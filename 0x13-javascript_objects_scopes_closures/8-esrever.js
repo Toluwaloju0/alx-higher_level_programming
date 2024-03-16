a6cd611c87bf8100d806c8d3b93920caf9a30bbb
Toluwaloju0 <teecoded1990@gmail.com>
2024-03-16 20:54:33 +0100

    javascript

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const newList = [];
-  for (let b = list.length; b >= 0; b--) {
+  for (let b = list.length - 1; b >= 0; b--) {
     newList.push(list[b]);
   }
   return newList;
