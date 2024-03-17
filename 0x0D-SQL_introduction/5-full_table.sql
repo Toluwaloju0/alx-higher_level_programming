8f205836dce83bae0ce71958c2323aee76d1ff96
Toluwaloju0 <teecoded1990@gmail.com>
2024-03-17 22:12:02 +0100

    database

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,13 +1,2 @@
 -- A script to show a full description of a table
-SELECT
-    COLUMN_NAME,
-    DATA_TYPE,
-    IS_NULLABLE,
-    COLUMN_DEFAULT,
-    COLUMN_TYPE,
-    COLUMN_COMMENT
-FROM
-    INFORMATION_SCHEMA.COLUMNS
-WHERE
-    TABLE_SCHEMA = 'hbtn_0c_0'
-    AND TABLE_NAME = 'first_table';
+SHOW FULL COLUMNS FROM first_table;
