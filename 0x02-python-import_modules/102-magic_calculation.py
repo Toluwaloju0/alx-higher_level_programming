38f01fcfb67d0ad4966da8eb23a5628ecaef2d15
Toluwaloju0 <teecoded1990@gmail.com>
2024-03-28 02:50:29 +0100

    python

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,8 @@
 def magic_calculation(a, b):
-    if a < b:
-        c = a + b
-        for i in range(1, 6):
-            c += i
-        return c
+    if a > b:
+        return (a + b)
     else:
-        return a - b
+        c = b - a
+        for i in range(4, 6):
+             c = magic_calculation(c, i)
+        return c * -1
