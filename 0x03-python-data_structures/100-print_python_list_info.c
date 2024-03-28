cec6f5790e643d835d3d519ccf687a28a1c3a331
Toluwaloju0 <teecoded1990@gmail.com>
2024-03-28 23:03:50 +0100

    cpython

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,31 @@
+#include <Python.h>
+#include <listobject.h>
+#include <object.h>
+#include <stdio.h>
+
+void print_python_list_info(PyObject *p)
+
+{
+	py_ssize_t list_size = Pylist_Size(p);
+	py_ssize_t a;
+	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+	PyObject *item;
+	const char *item_type;
+
+
+	printf("[*] Size of the Python List = %zd\n", list_size);
+	printf("[*] Allocated = %zd\n", allocated)
+
+	for (a = 0; a < list_size; a++)
+	{
+		item = PyList_GetItem(p, i);
+		item = PyObject_Type(item);
+		item = PyObject_GetAttrString(item, "__name__");
+		item = PyObject_Str(item);
+		item = PyUnicode_AsEncodedString(item, "utf-8", "strict");
+		item_type = PyBytes_AsString(item);
+		printf("Element %zd: %s\n", a, item_type);
+
+		Py_DECREF(item)
+	}
+}
