693c25e5e0beafbb844332da95d43844977717da
Toluwaloju0 <teecoded1990@gmail.com>
2024-04-03 22:16:38 +0100

    tests

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -2,9 +2,8 @@
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 matrix = [
-    [1.66, 2.67, 3.4],
-    [4, 5, 6],
-    7
+    [1, 2, 3],
+    [4, 5, 6]
 ]
 print(matrix_divided(matrix, 3))
 print(matrix)
