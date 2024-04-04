3cec1055c735b557f357e70e65a5e85b34238149
Toluwaloju0 <teecoded1999@gmail.com>
2024-04-04 09:32:19 +0100

    tests:

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,16 +2,34 @@
 """A module to print a text"""
 
 
-def text_indentation(text):
+def text_indentation(text=""):
     """A function to seperare a string based on characters
     Args:
         text(str): the text to be seperated
     """
 
+    a = 0
+    if text is None:
+        return None
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    for a in range(len(text)):
+    if len(text) == 0:
+        return None
+    if text[0] == ' ':
+        a = a + 1
+        while text[a]:
+            if text[a] == ' ':
+                a = a + 1
+                continue
+            break
+    b = len(text) - 1
+    while b > 0:
+        if text[b] == ' ':
+            b = b - 1
+            continue
+        break
+    while a <= b:
         if text[a] == '.' or text[a] == '?' or text[a] == ':':
             print(text[a] + '\n')
             a = a + 1
@@ -21,4 +39,8 @@ def text_indentation(text):
                     continue
                 break
             continue
+        if a == b:
+            print(text[a])
+            return None
         print(text[a], end='')
+        a = a + 1
