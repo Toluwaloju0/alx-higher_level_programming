8cbdabdefce2500dcfff04c363b4832c74754631
Toluwaloju0 <teecoded1990@gmail.com>
2024-04-04 22:53:28 +0100

    python unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,7 +9,7 @@ class TestMaxInteger(unittest.TestCase):
     def test_max(self):
         #To test max integers for numbers
         self.assertAlmostEqual(max_integer([1, 2, 3, 4, 7, 8, 5, -1]), 8)
-        self.assertAlmostEqual(max_integer([10, 40, 200, 180, 199]), 200)
+        self.assertAlmostEqual(max_integer([10, 40, 20, 180, 200]), 200)
         self.assertAlmostEqual(max_integer([200, 40, 10, 180, 199]), 200)
         self.assertAlmostEqual(max_integer([1, 40, 10, 18, -3]), 40)
         self.assertAlmostEqual(max_integer([10]), 10)
