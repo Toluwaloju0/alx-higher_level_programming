503e254fdf970adfa08904b3529aee00be6b9d88
Toluwaloju0 <teecoded1999@gmail.com>
2024-04-07 22:28:29 +0100

    inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/vin/python3
+"""A module to dedine a class square"""
+
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """A class that inherits from rectangle"""
+
+    def __init__(self, size):
+        """A function to initialize th xlass"""
+
+        self.integer_validator("size", size)
+
+        super().__init__(size, size)
