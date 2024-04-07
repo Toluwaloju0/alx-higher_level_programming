4dbd340859cf3bd245ba3ba65421a9e52f6ae774
Toluwaloju0 <teecoded1999@gmail.com>
2024-04-07 22:32:15 +0100

    inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,9 +4,9 @@
 
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """A class that inherits from rectangle"""
-
 
     def __init__(self, size):
         """A function to initialize th xlass"""
