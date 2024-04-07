ad4894303d88fbe4b4d6e162ab9d7f5b0c9d2a9a
Toluwaloju0 <teecoded1999@gmail.com>
2024-04-07 16:37:44 +0100

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,6 +9,6 @@ def inherits_from(obj, a_class):
         a_class: the class to be teste against
     """
 
-    if type(obj) is a_class:
+    if isinstance(obj, (a_class)):
         return True
     return False
