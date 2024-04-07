d1fbc731660e7bb0086669033c83dcd0a00d91a9
Toluwaloju0 <teecoded1999@gmail.com>
2024-04-07 20:12:09 +0100

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""A module to check if a class \
-inherits fromm another class"""
+"""A module to check if a class inherits fromm another class"""
 
 
 def inherits_from(obj, a_class):
-    """A function to xhexk if a class is a subclass of another
+    """A function to check if an object is a subclass of another
     Args:
-        obj: The onject to check
-        a_class: the class to be teste against
+        obj: The object to check
+        a_class: the class to be tested against
     """
 
-    if isinstance(obj, a_class);
+    if isinstance(obj, a_class):
          return True
     return False
