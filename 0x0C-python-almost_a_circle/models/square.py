f374c92f87e21c2d56ded03af320443049ea34e9
Toluwaloju0 <teecoded1990@gmail.com>
2024-04-10 16:17:23 +0100

    almost a circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,6 +12,11 @@ class Square(Rectangle):
         """A function to initialize a square class"""
 
         super().__init__(size, size, x, y, id)
+        if type(size) is not int:
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
+        self.__size = size
 
     def __str__(self):
         """To get a string of the class"""
@@ -19,3 +24,19 @@ class Square(Rectangle):
         a = "[Square] ({}) ".format(self.id)
         b = "{}/{} - {}".format(self.x, self.y, self.width)
         return a + b
+
+    @property
+    def size(self):
+        """To get the value of size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """To set the value of size"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+            self.__size = value
