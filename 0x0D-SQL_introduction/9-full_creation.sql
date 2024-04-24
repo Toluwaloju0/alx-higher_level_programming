61c141ed4ffe5395f6231a9fa043db29f8430cd5
Toluwaloju0 <teecoded1990@gmail.com>
2024-04-24 23:52:20 +0100

    sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,12 +1,11 @@
 -- A script to create a second table along with it values
 CREATE TABLE IF NOT EXISTS second_table (
-    id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256),
     score INT,
-    PRIMARY KEY(id)
     );
 
-INSERT IGNORE INTO second_table(name, score, id) VALUES
+INSERT IGNORE INTO second_table(name, score) VALUES
     ('John', 10),
     ('Alex', 3),
     ('Bob', 14),
