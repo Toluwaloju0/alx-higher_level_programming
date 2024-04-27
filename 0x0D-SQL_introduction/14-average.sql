34f7c9d9b52b52df52122c1dc9fbc7e4a2968e0e
Toluwaloju0 <teecoded1990@gmail.com>
2024-04-27 03:11:39 +0100

    sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,5 +1,2 @@
---A script to add a new column for the average scores
-ALTER TABLE second_table
-  ADD COLUMN (average INT);
-INSERT INTO second_table VALUES
-  SELECT AVG score FROM second_table;
+-- A script to add a new column for the average scores
+SElECT AVG(score) AS average FROM second_table;
