b991acd35590eab3d998a29a5b52cd027067f1d7
Toluwaloju0 <teecoded1999@gmail.com>
2024-08-03 17:08:29 +0100

    python networking

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that show the methods a url allows
-curl -s --request ALLOW "$1"
+curl -s --request OPTIONS "$1"
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script to get a url and set a variable
-curl -sI -H "Content-Length: 98" "$1"
+curl -s -H "X-School-User-Id: 98" "$1"
