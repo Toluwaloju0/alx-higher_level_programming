c0a9219bda844ee571ce721ba928f2922976f288
Toluwaloju0 <teecoded1999@gmail.com>
2024-08-04 20:24:33 +0100

    python

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,9 +2,9 @@
 """A module to request a url and print the header"""
 
 import sys
-import urllib
+import urllib.request
 
 
 with urllib.request.urlopen(sys.argv[1]) as response:
-    opened_url = response.read()
-for 
+    page_header = response.info()
+    print(page_header['X-Request-Id'])
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module to make a post request"""
+
+
+import urllib.request, urllib.parse
+import sys
+
+
+url = sys.argv[1]
+values = {'email': sys.argv[2]}
+data = urllib.parse.encode(values)
+data = data.encode('ascii')
+request = urllib.request.Request(url, data)
+with urllib.request.urlopen(request) as response:
+    page = response.read()
+    print((page)
