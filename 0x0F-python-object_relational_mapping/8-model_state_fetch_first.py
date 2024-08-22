61c5c35b715b828d53de3ceef0055106ad7ba4e1
Toluwaloju0 <teecoded1990@gmail.com>
2024-08-22 23:31:04 +0100

    sql alchemy

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,7 +2,7 @@
 """ A module to select everting in a database using sqlalchemy"""
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from model_state import Base, State
 from sys import argv
 
@@ -10,8 +10,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/\
 {}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = Session(bind=engine)
 
     result = session.query(State).order_by(State.id).first()
     print("{}: {}".format(result.id, result.name))
