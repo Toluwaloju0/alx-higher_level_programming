1febe9c3936a93a4b657b439ed861572d32fd710
Toluwaloju0 <teecoded1990@gmail.com>
2024-08-22 23:42:38 +0100

    sql alchemy

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,8 +15,7 @@ if __name__ == "__main__":
     result = session.query(State).order_by(State.id).first()
     if result is None:
         print("Nothing")
-        session.close()
-        return
-    print("{}: {}".format(result.id, result.name))
+    else:
+        print("{}: {}".format(result.id, result.name))
 
     session.close()
