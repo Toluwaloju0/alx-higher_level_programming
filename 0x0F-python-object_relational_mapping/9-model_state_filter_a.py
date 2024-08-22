22bf698bd70d0241a298cfa5368e7c654ad9d512
Toluwaloju0 <teecoded1990@gmail.com>
2024-08-22 23:46:17 +0100

    sql alchemy

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """ A module to select everting in a database using sqlalchemy"""
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from sys import argv
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/\
 {}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = Session(bind=engine)
 
     result = session.query(State).order_by(State.id).\
         filter(State.name.like('%a%')).all()
