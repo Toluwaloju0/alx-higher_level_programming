8f5144f5409a3524350fddf637e819738e060681
Toluwaloju0 <teecoded1990@gmail.com>
2024-08-22 23:52:36 +0100

    sql alchemy

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
 """ A module to select everting in a database using sqlalchemy"""
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from sys import argv
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/\
 {}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = Session(bind=engine)
 
-    try:
-        result = session.query(State).order_by(State.id).\
-            filter(State.name == argv[4]).one()
+    result = session.query(State).order_by(State.id).\
+        filter(State.name == argv[4]).one_or_none()
 
-        print(result.id)
-    except NoResultFound:
+    if result is None:
         print("Not found")
+    else:
+        print(result.id)
     session.close()
