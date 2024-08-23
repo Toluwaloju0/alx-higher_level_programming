ec90de71f942781c199ded003e5f336c7457d465
Toluwaloju0 <teecoded1990@gmail.com>
2024-08-23 05:29:37 +0100

    sql alchemy

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 """ A moduole to delete a column in a table"""
 
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+
 
 if __name__ == '__main__':
-    engine = create_engine("mysql+mysqldb://{}:{}/\
-@localhost:3306/{}".format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/\
+{}".format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = Session(bind=engine)
 
-    rows = session.query(State).filter(State.name.like('%a%'))
-    row_to_del = rows.all()
+    row_to_del = session.query(State).filter(State.name.like('%a%')).all()
     for row in row_to_del:
-        print(row.name)
         session.delete(row)
     session.commit()
     session.close()
