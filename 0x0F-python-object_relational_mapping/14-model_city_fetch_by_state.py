28791d24fafaf562d4ae53108d8dd11322f7ca3b
Toluwaloju0 <teecoded1990@gmail.com>
2024-08-23 05:59:10 +0100

    sql alchemy

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """ A module to print objects in a cities table"""
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, registry
-from sys import argv
-from model_state import Base, State
 from model_city import City
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
 
-if __name__ =='__main__':
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"\
-        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    mapper_registry = registry()
-    mapper_registry.configure()
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/\
+{}".format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    
-    result = session.query(State, City).join(City, City.state_id==State.id).all()
-    for State, City in result:
-        print("{}: ({}) {}".format(State.name, City.id, City.name))
+    Base.metadata.create_all(engine)
+
+    session = Session(bind=engine)
+
+    result = session.query(State, City).filter(State.id == City.state_id).all()
+    for S, C in result:
+        print("{}: ({}) {}".format(S.name, C.id, C.name))
