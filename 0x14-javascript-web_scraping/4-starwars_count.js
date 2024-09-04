d0e9d87c6733e43dd6bce01699c6b7fed932fd96
Toluwaloju0 <teecoded1990@gmail.com>
2024-09-04 19:26:56 +0100

    javascript

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+const { argv } = require('node:process');
+const keyWord = 'https://swapi-api.alx-tools.com/api/people/18/';
+let time = 0;
+let a;
+
+request(`${argv[2]}`, function (err, res, body) {
+  if (!err && res.statusCode === 200) {
+    const bodyDict = JSON.parse(body);
+    for (a = 0; a < bodyDict.count; a++) {
+      if (bodyDict.results[a].characters.includes(keyWord)) {
+        time++;
+      }
+    }
+    console.log(time);
+  }
+});
