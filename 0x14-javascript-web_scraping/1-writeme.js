940c0abbdbfdbcac3f0c814a85c7b45dbb2bb47c
Toluwaloju0 <teecoded1990@gmail.com>
2024-09-04 18:05:06 +0100

    javascript

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const { argv } = require('node:process');
+
+fs.writeFile(`${argv[2]}`, `${argv[3]}`, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
